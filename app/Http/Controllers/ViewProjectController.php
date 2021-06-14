386efab53308921e808cbaa8b4bf0e10e471d33d
ZihadurRahman <zihadurrahman24@gmail.com>
2021-06-14 14:17:47 +0800

    Update ViewProjectController.php

--- app/Http/Controllers/ViewProjectController.php
+++ app/Http/Controllers/ViewProjectController.php
@@ -3,13 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\BigProject;
+use App\Models\SubProject;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ViewProjectController extends Controller
 {
     public function index ()
     {
-        return view('projects.view');
+        // $users = DB::table('users')
+        //         ->select(DB::raw('count(*)as user_count,status'))
+        //         ->where ('status','<>',1)
+        //         ->groupBy('status')
+        //         ->get();
+        // $users = User::where('status','<>',1)
+        // ->groupBy('status')
+        // ->get('name','status');
+        // $users_count = $users->count();
+
+        // $users= User::get();
+        $bigProjects = BigProject::get();
+        $SubProjects = SubProject::get();
+        return view('projects.view',[
+        'user' => $user,
+        'managers' => $managers,
+        'admins' => $admins,
+        'viewers' => $viewers,
+        'userIsManager' => $userIsManager,
+        'userIsAdmin' => $userIsAdmin,
+        'userIsViewer' => $userIsViewer,
+    ]);
     }
 
     public function something()
@@ -19,7 +44,3 @@ class ViewProjectController extends Controller
         return redirect()->route('addadmin');
     }
 }
-
-
-
-

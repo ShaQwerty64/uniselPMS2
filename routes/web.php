29aeccaac9ac87314e69495dc27ca4ed3d24e304
ShaQwerty64 <83680366+ShaQwerty64@users.noreply.github.com>
2021-07-12 16:52:48 +0800

    Backup for future

--- routes/web.php
+++ routes/web.php
@@ -48,9 +48,54 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function (){
     });
 
 
-
     //Top-management only
     Route::get('/viewer', [ViewProjectController::class,'index'])->middleware('permission:view projects')->name('view');
 });
 
-
+//  --------- Backup - from vendor\laravel\framework\src\Illuminate\Http\Request.php ------------
+/**
+     * By Shamim, for @livewire('banner')
+     * $theme =
+     * s:success,
+     * w:warning,
+     * d:danger,
+     *  :message,
+     * .:do not sent message
+     * @param  string  $message
+     * @param  string  $theme
+     */
+    // public function banner(
+    //     string $message,
+    //      string $theme = '',
+    //      int $admin = null,
+    //      int $user = null,
+    //      int $big = null,
+    //      int $sub = null,
+    //      string $PTJ = null,
+    //      int $oldBig = null,
+    //      string $oldPTJ = null)
+    // {
+    //     if ($admin != null || $user != null || $big != null || $sub != null || $PTJ != null){
+    //         $PH = new ProjectsHistory;
+    //         $PH->all_admin  = $admin == -100;
+    //         $PH->admin_id   = $admin == -100 ? null : $admin;
+    //         $PH->user_id    = $user;
+    //     $PH->big_project_id = $big;
+    //     $PH->sub_project_id = $sub;
+    //         $PH->PTJ        = $PTJ;
+    //         $PH->details    = $message;
+    //         $PH->save();
+    //     }
+    //     if ($oldBig != null || $oldPTJ != null){
+    //         $PH2 = new ProjectsHistory;
+    //         $PH2->all_admin  = false;
+    //         $PH2->big_project_id= $oldBig;
+    //         $PH2->PTJ           = $oldPTJ;
+    //         $PH2->details       = $message;
+    //         $PH2->save();
+    //     }
+    //     if ($theme != '.'){
+    //         $this->session()->put('banner.m', $message);
+    //         $this->session()->put('banner.t', $theme);
+    //     }
+    // }
